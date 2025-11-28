be4ec22e383ddfbf6de6949e4a701d447e310802
yahyaelkaed <129843158+yahyaelkaed@users.noreply.github.com>
2025-11-28 15:09:49 +0100

    Add files via upload

---
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package tn.esprit.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
+
+@TestComponent
+class StudentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Context Loads Successfully");
+    }
+
+}
