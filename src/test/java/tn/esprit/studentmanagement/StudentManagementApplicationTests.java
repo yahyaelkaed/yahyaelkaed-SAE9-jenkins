dc5a4b03775009bfe2b9d727fc95bfccbc078e53
yahyaelkaed <129843158+yahyaelkaed@users.noreply.github.com>
2025-12-05 13:26:30 +0100

    Replace @TestComponent with @SpringBootTest

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,9 +2,8 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestComponent;
 
-@TestComponent
+@SpringBootTest
 class StudentManagementApplicationTests {
 
     @Test
